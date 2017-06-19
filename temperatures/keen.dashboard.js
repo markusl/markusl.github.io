c5fedaaab1b1a162aede3da608b7dd350ce20c65
Markus Lindqvist <markus.lindqvist@reaktor.fi>
2017-06-19 11:20:40 +0300

    Improve graphs

--- temperatures/keen.dashboard.js
+++ temperatures/keen.dashboard.js
@@ -3,18 +3,20 @@ const client = new Keen({
   readKey: "5DF6E48C5E39D7F3AE068D332FCA76A23CAAD2041B8B079F0F2B5B23E7297036E3B6C0926FD714366A5EC67A2E1A5B152FCF8EB54C341E38A6C450E2DD283D4FA6DCCA3349AE426E544F81C697DF74C981E8EC0D911AF7A223B65F34919A0C7C"
 });
 
-Keen.ready(function() {
-
-  // ----------------------------------------
-  // Average temperatures chart
-  // ----------------------------------------
-  const average_temperature = new Keen.Query("average", {
-    eventCollection: "values",
-    timeframe: "last_30_days",
-    targetProperty: "value",
-    interval: "hourly"
-  });
-  client.draw(average_temperature, document.getElementById("chart-01"), {
+const average_temperature_30_days = new Keen.Query("average", {
+  eventCollection: "values",
+  timeframe: "last_30_days",
+  targetProperty: "value",
+  interval: "hourly"
+});
+const average_temperature_24_hours = new Keen.Query("average", {
+  eventCollection: "values",
+  timeframe: "last_day",
+  targetProperty: "value",
+  interval: "hourly"
+});
+const drawTemperature = function(data, chartId) {
+  client.draw(data, document.getElementById(chartId), {
     chartType: "areachart",
     title: false,
     height: 250,
@@ -26,9 +28,15 @@ Keen.ready(function() {
         height: "85%",
         left: "5%",
         top: "5%",
-        width: "80%"
+        width: "100%"
       }
     }
   });
+};
+const drawTemperature24hourAverage = () => drawTemperature(average_temperature_24_hours, "chart-01");
+const drawTemperature30DayAverage = () => drawTemperature(average_temperature_30_days, "chart-02");
 
-});
+Keen.ready(drawTemperature30DayAverage);
+$( window ).resize(drawTemperature30DayAverage);
+Keen.ready(drawTemperature24hourAverage);
+$( window ).resize(drawTemperature24hourAverage);
